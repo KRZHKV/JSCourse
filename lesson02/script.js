6944eb3421feb21d07310471dc8cb3c8250950fd
Kirill Kruzhkov <Kruzhkovll@gmail.com>
2020-01-24 10:17:23 +0300

    Задание 2

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,2 +1,20 @@
-let i = 5;
-console.log(++i);
+let money = 200000;
+let income = '50000';
+let addExpenses = 'Такси, Фастфуд, ЖКХ, Шоколадки';
+let deposit = true;
+let mission = 80000000;
+let period = 8;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
