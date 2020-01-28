681e747bdc5038f0da8af8396e95156041513282
Kirill Kruzhkov <Kruzhkovll@gmail.com>
2020-01-28 19:31:41 +0300

    Четвертый урок

---
+++ lesson04/easy/script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let money = 200000;
+let income = '50000';
+let addExpenses = 'Такси, Фастфуд, ЖКХ, Шоколадки';
+let deposit = true;
+let mission = 800000;
+let period = 8;
+
+let showTypeOf = function(data) {
+	console.log(data, typeof(data));
+}
+
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+money = +prompt('Ваш ежемесячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую');
+
+let expenses1 = prompt('Введите обязательную статью расходов');
+let amount1 = +prompt('Во сколько вам это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов');
+let amount2 = +prompt('Во сколько вам это обойдется?');
+
+
+let getExpensesMonth = function(num1, num2) {
+	return num1 + num2;
+}
+
+let getAccumulatedMonth = function(money, expenses) {
+	return money - expenses;
+}
+
+let getTargetMonth = function(target, money) {
+	let sum = target / money;
+	return console.log(sum);
+}
+
+console.log(getExpensesMonth(amount1, amount2));
+getAccumulatedMonth(money, getExpensesMonth(amount1, amount2));
+let accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth(amount1, amount2));
+getTargetMonth(mission, accumulatedMonth);
+
+let missionDone = Math.round(mission / accumulatedMonth);
+console.log('Цель будет достигнута за' + ' ' + missionDone + ' ' + 'месяцев');
+
+let budgetDay = parseInt(accumulatedMonth / 30); //Вырезает из строки числа ТОЛЬКО ЦЕЛЫЕ
+console.log('Бюджет на день' + ' ' + budgetDay);
+
+let getStatusIncome = function() {
+	if(budgetDay > 1200) {
+		return ('У вас высокий уровень дохода');
+	} else if(budgetDay >= 600 && budgetDay <= 1200) {
+		return ('У вас средний уровень дохода');
+	} else if(budgetDay < 600 && budgetDay > 0) {
+		return ('К сожалению, у вас уровень дохода меньше среднего');
+	} else if(budgetDay <= 0) {
+		return ('Что-то пошло не так');
+	}
+}
+
+console.log(getStatusIncome());
