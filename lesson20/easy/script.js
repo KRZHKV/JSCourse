942bbc84ef60ff204b3203807404c5f20d1840c6
krkirian <kruzhkovll@gmail.com>
2020-02-26 12:25:41 +0300

    19 урок исправления

--- lesson20/easy/script.js
+++ lesson20/easy/script.js
@@ -67,9 +67,11 @@ window.addEventListener('DOMContentLoaded', function () {
                     let target = event.target;
                     if (target.classList.contains('close-btn')) {
                         handlerMenu();
-                    } else if (target.closest('a')) {
-                        handlerMenu();
-                        
+                    } else {
+                        target = target.closest('li');
+                        if (!target) {
+                            handlerMenu;
+                        }
                     }
                 })
 
@@ -168,4 +170,121 @@ window.addEventListener('DOMContentLoaded', function () {
 
                 }
                 tabs();
+
+                //Слайдер
+
+                const slider = () => {
+                    const slide = document.querySelectorAll('.portfolio-item'),
+                          btn = document.querySelectorAll('.portfolio-btn'),
+                          dot = document.querySelectorAll('.dot'),
+                          slider = document.querySelector('.portfolio-content'),
+                          dots = document.querySelector('.portfolio-dots');
+
+
+                    let currentSlide = 0,
+                        interval;
+                    
+                    const createDots = () => {
+                        const newDot = document.createElement('li');
+                        newDot.classList.add('dot');
+                        dots.appendChild(newDot);
+                        dots.appendChild(newDot);
+                        dots.appendChild(newDot);
+                        dots.appendChild(newDot);
+                        dots.appendChild(newDot);
+                        dots.appendChild(newDot);
+
+                    }
+                    const createDotsActive = () => {
+                        const newDot = document.createElement('li');
+                        newDot.classList.add('dot');
+                        newDot.classList.add('dot-active');
+                        dots.appendChild(newDot);
+
+                    }
+                    createDotsActive();
+                    createDots();
+                    const prevSlide = (elem, index, strClass) => {
+                        elem[index].classList.remove(strClass);
+                    };
+
+                    const nextSlide = (elem, index, strClass) => {
+                        elem[index].classList.add(strClass);
+                    };
+
+                    const autoPlaySlide = () => {
+                        prevSlide(slide, currentSlide, 'portfolio-item-active');
+                        prevSlide(dot, currentSlide, 'dot-active');
+                        currentSlide++;
+                        if(currentSlide >= slide.length) {
+                            currentSlide = 0;
+                        }
+                        nextSlide(slide, currentSlide, 'portfolio-item-active');
+                        nextSlide(dot, currentSlide, 'dot-active');
+                    };
+
+                    const startSlide = (time = 3000) => {
+                        interval = setInterval(autoPlaySlide, time);
+
+                    };
+
+                    const stopSlide = ( ) => {
+                        clearInterval(interval);
+                    }
+
+                    slider.addEventListener('click', (event) => {
+                        event.preventDefault();
+
+                        let target = event.target;
+
+                        if(!target.matches('.portfolio-btn, .dot')) {
+                            return;
+                        }
+
+                        prevSlide(slide, currentSlide, 'portfolio-item-active');
+                        prevSlide(dot, currentSlide, 'dot-active');
+
+                        if(target.matches('#arrow-right')) {
+                            currentSlide++;
+                        }else if(target.matches('#arrow-left')) {
+                            currentSlide--;
+                        } else if (target.matches('.dot')) {
+                            dot.forEach( (elem, index) => {
+                                if (elem === target) {
+                                    currentSlide = index;
+                                }
+                            });
+                        }
+                        if(currentSlide >= slide.length) {
+                            currentSlide = 0;
+                        }
+                        if(currentSlide < 0) {
+                            currentSlide =  slide.length - 1;
+                        }
+                        nextSlide(slide, currentSlide, 'portfolio-item-active');
+                        nextSlide(dot, currentSlide, 'dot-active');
+                    });
+
+                    slider.addEventListener('mouseover', (event) => {
+                        if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                            stopSlide();
+                        }
+                    });
+
+                    slider.addEventListener('mouseout', (event) => {
+                        if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                            startSlide();
+                        }
+                    })
+
+
+                    startSlide(1500);
+
+
+
+                }
+
+                slider();
+
+
             });
