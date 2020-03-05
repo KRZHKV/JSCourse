abe4c474b7be42ead506cd6f05d55d64e5df0154
krkirian <kruzhkovll@gmail.com>
2020-03-05 14:08:18 +0300

    27 урок исправления

--- lesson27/src/modules/toggleMenu.js
+++ lesson27/src/modules/toggleMenu.js
@@ -2,7 +2,7 @@ const toggleMenu = () => {
     const btnMenu = document.querySelector('.menu'),
         menu = document.querySelector('menu'),
         btnClose = document.querySelector('.close-btn'),
-        menuItem = menu.querySelectorAll('ul>li');
+        menuItem = menu.querySelectorAll('ul>li>a');
 
     const handlerMenu = () => {
         menu.classList.toggle('active-menu');
@@ -14,10 +14,12 @@ const toggleMenu = () => {
         if (target.classList.contains('close-btn')) {
             handlerMenu();
         } else {
-            target = target.closest('li');
-            if (!target) {
-                handlerMenu;
-            };
+            menuItem.forEach((elem) => {
+                if( elem === target ) {
+                handlerMenu();
+                };
+            });
+            
         };
     });
 
